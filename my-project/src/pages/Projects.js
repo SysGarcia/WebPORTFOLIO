a174256c8d73da775eb38c25601f4d7978c9bfc0
Wolfuliam <isaacbvgf88@gmail.com>
2025-01-12 15:45:54 +0100

    Cv Changes

---
+++ my-project/src/pages/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopBar from '../components/Header/HeaderComponents/TopBar'; // Asegúrate de que la ruta sea correcta
+
+function Blog() {
+  // Establece el valor de current_page según la ruta activa
+  const current_page = '/projects';
+
+  return (
+    <div>
+      <TopBar current_page={current_page} /> 
+    </div>
+  );
+}
+
+export default Blog;
